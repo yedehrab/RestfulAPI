0e2a3b5b5c42241f98eb711695813afca829962c
yedehrab <yyunussemree@gmail.com>
2018-08-10 22:57:41 +0300

    +

--- v1.0.3/README.md
+++ v1.0.3/README.md
@@ -18,3 +18,78 @@ curl localhost:3000/foo?kelime=selam
 * Payloadların, headerların incelenmesi
 > HTTP isteklerinin HTTP sunucusuna dize (string) olarak gelmesi
 > Postman adlı uygulama ile body kısmına yazdığım raw içerisindeki metinlerdir.
+
+* Request - Response yapısı
+> Sunucuya istek göderme ve isteğe karşılık sunucunun tepki vermesi
+
+## Önemli bilgiler
+### Kodun içinde yorum satırları ile açıklanmıştır. Bazıları;
+
+```javascript
+// Sunucu her isteğe string ile karşılık vermeli
+var sunucu = http.createServer(function (istek, yanıt) {
+```
+
+```Javascript
+/**
+ * Url ayrıştırma işlemi
+ * Örnek: {... query: {}, pathname: '/ornek' ... } şeklinde bir url classı
+ */
+var ayrıştırılmışUrl = url.parse(istek.url, true);
+```
+
+```Javascript
+/**
+ * Sorgu kelimesini (query string) obje olarak almak.
+ * Örnek: "curl localhost:3000/foo?test=testtir" ise { test : 'testtir' }
+ * Not: "?test=testtir" sorgu dizesidir.
+ */
+var sorguDizesiObjeleri = ayrıştırılmışUrl.query;
+```
+
+```Javascript
+/**
+ * Ayrıştırılan urldeki pathname değişkenindeki değeri yol'a alıyorz.
+ * Örnek: 'curl localhost:3000/ornek/test/' => yolu '/ornek/test/'
+ * Not: sorgu dizeleri ele alınmaz ( 'curl localhost:3000/ornek?foo=bar' => yolu '/ornek' )
+ */
+var yol = ayrıştırılmışUrl.pathname;
+```
+
+```Javascript
+/**
+ * Replace içinde verilen işaretler çıkartılarak alınan yol. 
+ * Örnek: '/ornek' -> 'ornek' veya '/ornek/test/' -> 'ornek/test/' olarak kırpılmakta. 
+ * Not: Sadece ilk karakter kırpılıyor (?)
+ */
+var kırpılmışYol = yol.replace(/^\/+|\+$/g, '');
+```
+
+```Javascript
+/**
+ * HTTP metodu alma
+ * Örnek: GET, POST, PUT, DELETE ...
+ */
+var metot = istek.method.toLowerCase();
+```
+
+```Javascript
+/**
+ * İsteğin içindeki başlıkları (header keys) obje olarak almak.
+ * Not: Postman ile headers sekmesinde gönderilen anahtarları (keys) 
+ * ve değerlerini (the value of them) içerir.
+ */
+var başlıklar = istek.headers;
+```
+
+```Javascript
+istek.on('end', function () {
+    // ...
+
+    /**
+     * Sonucu döndürme
+     */
+    yanıt.writeHead(durumKodu);
+    yanıt.end(yükDizesi);
+});
+```
--- v1.0.3/index.js
+++ v1.0.3/index.js
@@ -11,40 +11,158 @@ var DizeÇözücü = require('string_decoder').StringDecoder;
 
 // Sunucu her isteğe string ile karşılık vermeli
 var sunucu = http.createServer(function (istek, yanıt) {
-    // URL'i alma ve inceleme
-    var incelenmişUrl = url.parse(istek.url, true);
+    /**
+     * Url ayrıştırma işlemi
+     * Örnek: {... query: {}, pathname: '/ornek' ... } şeklinde bir url classı
+     */
+    var ayrıştırılmışUrl = url.parse(istek.url, true);
 
-    // Yolu alma
-    var yol = incelenmişUrl.pathname; // Tam yoldur.
-    var kırpılmışYol = yol.replace(/^\/+|\+$/g, '') // Solda verilen işaretler çıkartılarak alınan yol.
+    /**
+     * Sorgu kelimesini (query string) obje olarak almak.
+     * Örnek: "curl localhost:3000/foo?test=testtir" ise { test : 'testtir' }
+     * Not: "?test=testtir" sorgu dizesidir.
+     */
+    var sorguDizesiObjeleri = ayrıştırılmışUrl.query;
 
-    // Sorgu kelimesini (query string) obje olarak almak.
-    var sorguKelimesiObjesi = incelenmişUrl.query;
+    /**
+     * Ayrıştırılan urldeki pathname değişkenindeki değeri yol'a alıyorz.
+     * Örnek: 'curl localhost:3000/ornek/test/' => yolu '/ornek/test/'
+     * Not: sorgu dizeleri ele alınmaz ( 'curl localhost:3000/ornek?foo=bar' => yolu '/ornek' )
+     */
+    var yol = ayrıştırılmışUrl.pathname;
 
-    // HTTP metodu alma
-    var metod = istek.method.toLowerCase();
+    /**
+     * Replace içinde verilen işaretler çıkartılarak alınan yol. 
+     * Örnek: '/ornek' -> 'ornek' veya '/ornek/test/' -> 'ornek/test/' olarak kırpılmakta. 
+     * Not: Sadece ilk karakter kırpılıyor (?)
+     */
+    var kırpılmışYol = yol.replace(/^\/+|\+$/g, '');
 
-    // Üst bilgileri (headersları) obje olarak almak
-    var üstBilgiler = istek.headers;
+    /**
+     * HTTP metodu alma
+     * Örnek: GET, POST, PUT, DELETE ...
+     */
+    var metot = istek.method.toLowerCase();
 
-    // Eğer payloads varsa onları almak.
+    /**
+     * İsteğin içindeki başlıkları (header keys) obje olarak almak.
+     * Not: Postman ile headers sekmesinde gönderilen anahtarları (keys) 
+     * ve değerlerini (the value of them) içerir.
+     */
+    var başlıklar = istek.headers;
+
+    /**
+     * ASCI kodlarını çözümlemek için kod çözücü tanımlama
+     * Not: 'utf-8' çözümleme yöntemidir
+     */
     var kodÇözücü = new DizeÇözücü('utf-8');
     var tampon = '';
 
+    /**
+     * İstek ile data geldiği zaman çalışan metot
+     * @param data ASCI kodları
+     */
     istek.on('data', function (data) {
+        /**
+         * ASCI kodlarını 'utf-8' formatında çözümlüyoruz.
+         * Ornek: 42 75 -> Bu [ 42 = B, 75 = u]
+         */
         tampon += kodÇözücü.write(data);
     });
 
     istek.on('end', function () {
+        /**
+         * Son kısmı ekliyoruz.
+         * Not: Şu anlık '' (?)
+         */
         tampon += kodÇözücü.end();
 
-        yanıt.end("Selam\n");
+        /** 
+         * İsteğin gideceği işleyiciyi seçme
+         * Örnek: yönlendirici[ornek], yönlendirici içindeki ornek adlı anahtarın değerini tutar. [ornek = isleyiciler.örnek]
+         */
+        var seçilmişİşleyici =
+            typeof (yönlendirici[kırpılmışYol]) !== 'undefined' ?
+                yönlendirici[kırpılmışYol] : işleyiciler.bulunamadı;
 
-        console.log("İstek şu payloadla alındı: ", tampon);
+        
+
+        /**
+         * İşleyiciye gönderilen veri objesi oluşturma
+         * Örnek: { 'kırpılmışYol' = 'ornek', sorguDizesiObjeleri = {}, metot = 'post', vs.}
+         */
+        var veri = {
+            'kırpılmışYol': kırpılmışYol,
+            'sorguDizesiObjeleri': sorguDizesiObjeleri,
+            'metot': metot,
+            'başlıklar': başlıklar,
+            'yükler': tampon
+        };
+
+        seçilmişİşleyici(veri, function (durumKodu, yükler) {
+            // Durum kodunu kullan veya varsayılanı ele al
+            durumKodu = typeof (durumKodu) == 'number' ? durumKodu : 200;
+
+            // Yükleri kullan yada varsayılanı ele al
+            yükler = typeof (yükler) == 'object' ? yükler : {};
+
+            // Yükleri dize'ye çevirme
+            var yükDizesi = JSON.stringify(yükler);
+
+            /**
+             * Sonucu döndürme
+             */
+            yanıt.writeHead(durumKodu);
+            yanıt.end(yükDizesi);
+
+
+            console.log("Yanıt: ", durumKodu, yükDizesi);
+        });
     });
 });
 
-// Sunucu başlatıyoruz ve onu 3000 portundan dinliyoruz.
+/**
+ * Sunucu başlatıyoruz ve onu 3000 portundan dinliyoruz.
+ * Örnek kullanım: curl localhost:3000 
+ * Not: Eğer 3000 yerine 500 yazsaydık, locakhost:500 yapacaktık.
+ */
 sunucu.listen(3000, function () {
     console.log("Sunucu 3000 portundan dinleniyor.");
 });
+
+/**
+ * İşleyicileri (handlers) tanımlama
+ * Örnek: işleyiciler.örnek
+ * Not: Buradaki işleyiciler.örnek, yönlendiricilerdeki 'ornek' öğesine atanıyor.
+ */
+var işleyiciler = {};
+
+/**
+ * İşleyici örneği
+ * Örnek: localhost:3000/ornek yazıldığında bu fonksiyon çalışır.
+ * Not: ornek, yönlendirici 'nin bir objesidir.
+ */
+işleyiciler.örnek = function (veri, geriÇağırma) {
+    // HTTP durumunu ve yüklerini geri çağırıyoruz.
+    geriÇağırma(406, { 'isim': 'başlık örneği' });
+};
+
+/**
+ * İşleyici bulunamaması durumunda çalışan metod
+ * Örnek: localhost:3000/ornek1 yazıldığında bu fonksiyon çalışır. [ornek1 tanımlı değil]
+ * Not: ornek1, yönlendirici'de tanımlı olmayan bir objesidir.
+ */
+işleyiciler.bulunamadı = function (veri, geriÇağırma) {
+    // HTTP hata kodunu geri çağırıyoruz.
+    geriÇağırma(404);
+};
+
+/**
+ * İstekler için yönlendirici tanımlama
+ * Örnek: localhost:3000/<değişken>
+ * Not: Türkçe karakter içeremez :( [Adres çubuğuna yazıldığından dolayı] 
+ */
+var yönlendirici = {
+    // localhost:3000/ornek
+    'ornek': işleyiciler.örnek
+};
